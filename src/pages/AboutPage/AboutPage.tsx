0507bdb0dd71fd3d5006f587ad3b492ac94d9dd1
Yonatan Yuryev <yonilight333@gmail.com>
2023-12-12 18:27:11 +0200

    Added router-dom and routing

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+type Props = {};
+
+const AboutPage: FC = (props: Props) => {
+  return <div>About Page</div>;
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+type Props = {};
+
+const MainPage: FC = (props: Props) => {
+  return <div>Main Page</div>;
+};
+
+export default MainPage;
